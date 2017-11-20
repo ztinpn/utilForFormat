ba559a0ea8e076ffe1e937e8a33bf34f311e05b9
ztinpn <769606726@qq.com>
2017-11-20 15:06:14 +0800

    增加统计功能

--- UtilForFormat.m
+++ UtilForFormat.m
@@ -1,16 +1,124 @@
+% matlab学位论文绘图辅助工具类
+% 作者：ztinpn
+% 项目地址：https://github.com/ztinpn/utilForFormat
+% 下载地址：https://github.com/ztinpn/utilForFormat/archive/master.zip
+
 classdef UtilForFormat < handle
     properties
-        
+        fontsize
+        fontname
+        fontweight
+        version
     end
     methods
+        % 宋体，bold, 10
+        % utilForFormat = UtilForFormat('宋体', 10, 'bold')
+        function utilForFormat = setDefaultFont(utilForFormat, fontname, fontsize, fontweight)
+            utilForFormat.fontsize = fontsize;
+            utilForFormat.fontname = fontname;
+            utilForFormat.fontweight = fontweight;
+        end
+        function utilForFormat = UtilForFormat(checkUpdate)
+            % http://rrss.sinaapp.com/static/html/utilforformat.html?v=1.0
+            utilForFormat.version = 1;
+            switch checkUpdate
+                case 'on'
+                    try
+                        ie = actxserver('internetexplorer.application');
+                        ie.Navigate('http://rrss.sinaapp.com/static/html/utilforformat.html?v=1.0');
+                    catch
+                        
+                    end
+                case 'off'
+            end
+            utilForFormat.setDefaultFont('宋体', 10, 'bold');
+        end
+        function setFont(utilForFormat,h)
+            set(h,'fontname',utilForFormat.fontname);
+            set(h,'fontweight',utilForFormat.fontweight);
+            set(h,'fontsize',utilForFormat.fontsize)
+        end
+        function h = colorbarPlot(utilForFormat,titleString)
+            h = colorbar;
+            h_title = get(h,'title');
+            set(h_title,'string',titleString);
+            utilForFormat.setFont(h_title);
+        end
+        
+        function dissertationPlotCore3d(utilForFormat,titleStr,xtitle,ytitle,ztitle, width_in_cm)
+            utilForFormat.setFont(title(titleStr));
+            utilForFormat.setFont(xlabel(xtitle));
+            utilForFormat.setFont(ylabel(ytitle));
+            utilForFormat.setFont(zlabel(ztitle));
+            
+            set(gcf,'units','centimeters');
+            position_gcf = get(gcf,'position');
+            width_2_division = width_in_cm;%10;7.8;
+            ratio = position_gcf(1,3)/width_2_division;
+            % ratio = [1,1,ratio,ratio];
+            % set(gca,'position',[0 0 position(1,3:4)/ratio]);
+            set(gcf,'position',[position_gcf(1,1:2) position_gcf(1,3:4)/ratio]);
+            %             utilForFormat.cutPadding();
+            %             set(gca,'units','centimeters');
+        end
+        function dissertationPlotCore(utilForFormat,titleStr,xtitle,ytitle,width_in_cm)
+            utilForFormat.setFont(title(titleStr));
+            utilForFormat.setFont(xlabel(xtitle));
+            utilForFormat.setFont(ylabel(ytitle));
+            
+            set(gcf,'units','centimeters');
+            position_gcf = get(gcf,'position');
+            width_2_division = width_in_cm;%10;7.8;
+            ratio = position_gcf(1,3)/width_2_division;
+            % ratio = [1,1,ratio,ratio];
+            % set(gca,'position',[0 0 position(1,3:4)/ratio]);
+            set(gcf,'position',[position_gcf(1,1:2) position_gcf(1,3:4)/ratio]);
+            %             utilForFormat.cutPadding();
+            %             set(gca,'units','centimeters');
+        end
+        function dissertationPlot3d(utilForFormat,titleStr,xtitle,ytitle,ztitle,width_in_cm)
+            utilForFormat.dissertationPlotCore3d(titleStr,xtitle,ytitle,ztitle,width_in_cm);
+            iter_times = 1;
+            for i_iter = 1:iter_times
+                ratio_w2h = utilForFormat.getWidthToHeightRatio();
+                %             position_gca = get(gca,'position');
+                %             ratio_ax = position_gca(1,3) / position_gca(1,4);
+                position_gcf = get(gcf,'position');
+                set(gcf,'position',[position_gcf(1,1:3) position_gcf(1,3)/ratio_w2h]);
+            end
+            utilForFormat.moveToScreenCenter();
+        end
+        function dissertationPlot(utilForFormat,titleStr,xtitle,ytitle,width_in_cm)
+            utilForFormat.dissertationPlotCore(titleStr,xtitle,ytitle,width_in_cm);
+            iter_times = 1;
+            for i_iter = 1:iter_times
+                ratio_w2h = utilForFormat.getWidthToHeightRatio();
+                %             position_gca = get(gca,'position');
+                %             ratio_ax = position_gca(1,3) / position_gca(1,4);
+                position_gcf = get(gcf,'position');
+                set(gcf,'position',[position_gcf(1,1:3) position_gcf(1,3)/ratio_w2h]);
+            end
+            utilForFormat.moveToScreenCenter();
+        end
+        function dissertationPlotNoAdjust(utilForFormat,titleStr,xtitle,ytitle,width_in_cm)
+            utilForFormat.dissertationPlotCore(titleStr,xtitle,ytitle,width_in_cm);
+            utilForFormat.moveToScreenCenter();
+        end
+        function setLineWidth(utilForFormat,lineWidth)
+            lineArr = utilForFormat.getObjectHandlersByType(gca,'line');
+            for ia = 1:length(lineArr)
+                set(lineArr(ia),'linewidth',lineWidth);
+            end
+        end
     end
     methods (Static)
         function demo()
+            utilForFormat = UtilForFormat();
             t = 0:0.05:1;
             strCell = cell(1,10);
             
             % 'cml': 先color，再 marker最后 line的顺序生成曲线样式
-            labelsForPlotLine = UtilForFormat.labelsForPlotLineGnrt('cml');
+            labelsForPlotLine = utilForFormat.labelsForPlotLineGnrt('cml');
             
             figure;
             hold on;
@@ -20,8 +128,8 @@ classdef UtilForFormat < handle
                 strCell{1,ia} = sprintf('曲线%d',ia);
             end
             legend(strCell);
-            UtilForFormat.setLineWidth(1.5);
-            UtilForFormat.dissertationPlot('标题','x轴','y轴',10);
+            utilForFormat.setLineWidth(1.5);
+            utilForFormat.dissertationPlot('标题','x轴','y轴',10);
             xVec = 1:100;
             yVec = 1:200;
             figure;
@@ -29,149 +137,17 @@ classdef UtilForFormat < handle
             axis equal;
             xlim([min(xVec),max(xVec)]);
             ylim([min(yVec),max(yVec)]);
-            UtilForFormat.colorbarPlot('值[单位]');
-            UtilForFormat.dissertationPlotNoAdjust('非自适应','x轴','y轴',10);
-            
+            utilForFormat.colorbarPlot('值[单位]');
+            utilForFormat.dissertationPlotNoAdjust('非自适应','x轴','y轴',10);
             figure;
             imagesc(xVec,yVec,rand(length(yVec),length(xVec)));
             axis equal;
             xlim([min(xVec),max(xVec)]);
             ylim([min(yVec),max(yVec)]);
-            UtilForFormat.colorbarPlot('值[单位]');
-            UtilForFormat.dissertationPlot('自适应','x轴','y轴',10);
+            utilForFormat.colorbarPlot('值[单位]');
+            utilForFormat.dissertationPlot('自适应','x轴','y轴',10);
         end
         
-        function setFont(h)
-            set(h,'fontname','宋体');
-            set(h,'fontweight','bold');
-        end
-        function h = colorbarPlot(titleString)
-            h = colorbar;
-            h_title = get(h,'title');
-            set(h_title,'string',titleString);
-            UtilForFormat.setFont(h_title);
-        end
-        function cutPadding()
-            ax = gca;
-            set(ax,'units','centimeters');
-            outerpos = get(ax,'OuterPosition');
-            ti = get(ax,'TightInset');
-            left = outerpos(1) + ti(1);
-            bottom = outerpos(2) + ti(2);
-            ax_width = outerpos(3) - ti(1) - ti(3);
-            ax_height = outerpos(4) - ti(2) - ti(4);
-            set(ax,'Position',[left bottom ax_width ax_height]);
-        end
-        function ratio = getWidthToHeightRatio()
-            %             pause(0.5);
-            frame = getframe(gcf);
-            im = frame.cdata;
-            im = double(rgb2gray(im));
-            [mm,nn,zz] = size(im);
-            upper = 1;
-            bgcolor = im(1,1);
-            for ia = 1:mm
-                if ~(im(ia,1) == bgcolor && var(im(ia,:)) == 0)
-                    break;
-                end
-            end
-            upper = ia - 1;
-            for ia = 1:mm
-                if ~(im(mm - ia + 1,1) == bgcolor && var(im(mm - ia + 1,:)) == 0)
-                    break;
-                end
-            end
-            lower = mm - ia + 1;
-            for ir = 1:nn
-                if ~(im(1,ir) == bgcolor && var(im(:,ir)) == 0)
-                    break;
-                end
-            end
-            left = ir - 1;
-            for ir = 1:nn
-                if ~(im(1,nn - ir + 1) == bgcolor && var(im(:,nn - ir + 1)) == 0)
-                    break;
-                end
-            end
-            right = nn - ir + 1;
-            display(sprintf('%d %d %d %d',upper, lower, left, right));
-            width = right - left + 1;
-            height = lower - upper + 1;
-            ratio = width / height;
-        end
-        function moveToScreenCenter()
-            pos = get(0,'MonitorPosition');
-            set(gcf,'units','pixel');
-            pos_gcf = get(gcf,'position');
-            left = (pos(1,3) - pos_gcf(1,3)) / 2;
-            top = (pos(1,4) - pos_gcf(1,4)) / 2;
-            set(gcf,'position',[left top pos_gcf(3:4)]);
-        end
-        function dissertationPlotCore3d(titleStr,xtitle,ytitle,ztitle, width_in_cm)
-            title(titleStr,'fontname','宋体','fontweight','bold');
-            xlabel(xtitle,'fontname','宋体','fontweight','bold');
-            ylabel(ytitle,'fontname','宋体','fontweight','bold');
-            zlabel(ztitle,'fontname','宋体','fontweight','bold');
-            
-            set(gcf,'units','centimeters');
-            position_gcf = get(gcf,'position');
-            width_2_division = width_in_cm;%10;7.8;
-            ratio = position_gcf(1,3)/width_2_division;
-            % ratio = [1,1,ratio,ratio];
-            % set(gca,'position',[0 0 position(1,3:4)/ratio]);
-            set(gcf,'position',[position_gcf(1,1:2) position_gcf(1,3:4)/ratio]);
-            %             UtilForFormat.cutPadding();
-            %             set(gca,'units','centimeters');
-        end
-        function dissertationPlotCore(titleStr,xtitle,ytitle,width_in_cm)
-            title(titleStr,'fontname','宋体','fontweight','bold');
-            xlabel(xtitle,'fontname','宋体','fontweight','bold');
-            ylabel(ytitle,'fontname','宋体','fontweight','bold');
-            
-            set(gcf,'units','centimeters');
-            position_gcf = get(gcf,'position');
-            width_2_division = width_in_cm;%10;7.8;
-            ratio = position_gcf(1,3)/width_2_division;
-            % ratio = [1,1,ratio,ratio];
-            % set(gca,'position',[0 0 position(1,3:4)/ratio]);
-            set(gcf,'position',[position_gcf(1,1:2) position_gcf(1,3:4)/ratio]);
-            %             UtilForFormat.cutPadding();
-            %             set(gca,'units','centimeters');
-        end
-        function dissertationPlot3d(titleStr,xtitle,ytitle,ztitle,width_in_cm)
-            UtilForFormat.dissertationPlotCore3d(titleStr,xtitle,ytitle,ztitle,width_in_cm);
-            iter_times = 1;
-            for i_iter = 1:iter_times
-                ratio_w2h = UtilForFormat.getWidthToHeightRatio();
-                %             position_gca = get(gca,'position');
-                %             ratio_ax = position_gca(1,3) / position_gca(1,4);
-                position_gcf = get(gcf,'position');
-                set(gcf,'position',[position_gcf(1,1:3) position_gcf(1,3)/ratio_w2h]);
-            end
-            UtilForFormat.moveToScreenCenter();
-        end
-        function dissertationPlot(titleStr,xtitle,ytitle,width_in_cm)
-            UtilForFormat.dissertationPlotCore(titleStr,xtitle,ytitle,width_in_cm);
-            iter_times = 1;
-            for i_iter = 1:iter_times
-                ratio_w2h = UtilForFormat.getWidthToHeightRatio();
-                %             position_gca = get(gca,'position');
-                %             ratio_ax = position_gca(1,3) / position_gca(1,4);
-                position_gcf = get(gcf,'position');
-                set(gcf,'position',[position_gcf(1,1:3) position_gcf(1,3)/ratio_w2h]);
-            end
-            UtilForFormat.moveToScreenCenter();
-        end
-        function dissertationPlotNoAdjust(titleStr,xtitle,ytitle,width_in_cm)
-            UtilForFormat.dissertationPlotCore(titleStr,xtitle,ytitle,width_in_cm);
-            UtilForFormat.moveToScreenCenter();
-        end
-        function setLineWidth(lineWidth)
-            lineArr = UtilForFormat.getObjectHandlersByType(gca,'line');
-            for ia = 1:length(lineArr)
-                set(lineArr(ia),'linewidth',lineWidth);
-            end
-        end
         %%
         function f=getObjectHandlersByType(objHandler,type)
             list=allchild(objHandler);
@@ -250,6 +226,62 @@ classdef UtilForFormat < handle
                         end
                     end
             end
+        end
+        function cutPadding()
+            ax = gca;
+            set(ax,'units','centimeters');
+            outerpos = get(ax,'OuterPosition');
+            ti = get(ax,'TightInset');
+            left = outerpos(1) + ti(1);
+            bottom = outerpos(2) + ti(2);
+            ax_width = outerpos(3) - ti(1) - ti(3);
+            ax_height = outerpos(4) - ti(2) - ti(4);
+            set(ax,'Position',[left bottom ax_width ax_height]);
+        end
+        function ratio = getWidthToHeightRatio()
+            %             pause(0.5);
+            frame = getframe(gcf);
+            im = frame.cdata;
+            im = double(rgb2gray(im));
+            [mm,nn,zz] = size(im);
+            upper = 1;
+            bgcolor = im(1,1);
+            for ia = 1:mm
+                if ~(im(ia,1) == bgcolor && var(im(ia,:)) == 0)
+                    break;
+                end
+            end
+            upper = ia - 1;
+            for ia = 1:mm
+                if ~(im(mm - ia + 1,1) == bgcolor && var(im(mm - ia + 1,:)) == 0)
+                    break;
+                end
+            end
+            lower = mm - ia + 1;
+            for ir = 1:nn
+                if ~(im(1,ir) == bgcolor && var(im(:,ir)) == 0)
+                    break;
+                end
+            end
+            left = ir - 1;
+            for ir = 1:nn
+                if ~(im(1,nn - ir + 1) == bgcolor && var(im(:,nn - ir + 1)) == 0)
+                    break;
+                end
+            end
+            right = nn - ir + 1;
+            display(sprintf('%d %d %d %d',upper, lower, left, right));
+            width = right - left + 1;
+            height = lower - upper + 1;
+            ratio = width / height;
+        end
+        function moveToScreenCenter()
+            pos = get(0,'MonitorPosition');
+            set(gcf,'units','pixel');
+            pos_gcf = get(gcf,'position');
+            left = (pos(1,3) - pos_gcf(1,3)) / 2;
+            top = (pos(1,4) - pos_gcf(1,4)) / 2;
+            set(gcf,'position',[left top pos_gcf(3:4)]);
         end
     end
 end
--- demo.m
+++ demo.m
@@ -1,10 +1,30 @@
-%% 复制前需要保证copy options已选择：
-%% preserve information
-%% transparent background
-%% match figure screen size
+%%
+% matlab学位论文绘图辅助工具demo
+% 作者：ztinpn
+% 项目地址：https://github.com/ztinpn/utilForFormat
+% 下载地址：https://github.com/ztinpn/utilForFormat/archive/master.zip
 
+%% 复制前需要保证copy options已选择：
+% preserve information
+% transparent background
+% match figure screen size
+
+clear variables;
+% clear classes;
 close all;
 
+%% 生成工具类对象 这句必须有，生成之后不用重复执行。
+utilForFormat = UtilForFormat('on');
+% on: 打开使用统计功能，大家可以在http://new.cnzz.com/v1/login.php?siteid=1254502417里（访客分析->地区\运营商）
+% 查看本工具的全国用户使用情况
+% off: 关闭使用统计功能
+
+%% 默认字体相关
+% 默认字体是'宋体', '10pt', 'bold'
+% 可通过 utilForFormat.setDefaultFont('宋体', 10, 'bold');进行修改
+
+%%
+close all;
 t = 0:0.05:1;
 numOfLines = 8;
 strCell = cell(1,numOfLines);
@@ -13,7 +33,7 @@ strCell = cell(1,numOfLines);
 % (1) 颜色，标记，线型顺序
 
 % 'cml': 先color，再 marker最后 line的顺序生成曲线样式
-labelsForPlotLine = UtilForFormat.labelsForPlotLineGnrt('cml');
+labelsForPlotLine = utilForFormat.labelsForPlotLineGnrt('cml');
 
 figure;
 hold on;
@@ -23,15 +43,15 @@ for ia = 1:numOfLines
     strCell{1,ia} = sprintf('曲线%d',ia);
 end
 legend(strCell);
-UtilForFormat.setLineWidth(1.5); % 设置线宽
-UtilForFormat.dissertationPlot(...
+utilForFormat.setLineWidth(1.5); % 设置线宽
+utilForFormat.dissertationPlot(...
     '样式优先级：颜色>标记>线型',... % 标题字符串
     'x轴',...
-    'y轴',... 
+    'y轴',...
     7.5 ...   % 固定宽度，单位cm
-);
+    );
 % (2) 样式优先级：标记>颜色>线型
-labelsForPlotLine = UtilForFormat.labelsForPlotLineGnrt('mcl');
+labelsForPlotLine = utilForFormat.labelsForPlotLineGnrt('mcl');
 
 figure;
 hold on;
@@ -41,8 +61,8 @@ for ia = 1:numOfLines
     strCell{1,ia} = sprintf('曲线%d',ia);
 end
 legend(strCell);
-UtilForFormat.setLineWidth(1.5);
-UtilForFormat.dissertationPlot('样式优先级：标记>颜色>线型','x轴','y轴',7.5);
+utilForFormat.setLineWidth(1.5);
+utilForFormat.dissertationPlot('样式优先级：标记>颜色>线型','x轴','y轴',7.5);
 %% 二维图
 % 非自适应高度
 xVec = 1:100;
@@ -52,13 +72,13 @@ imagesc(xVec,yVec,rand(length(yVec),length(xVec)));
 axis equal;
 xlim([min(xVec),max(xVec)]);
 ylim([min(yVec),max(yVec)]);
-UtilForFormat.colorbarPlot('值[单位]');
-UtilForFormat.dissertationPlotNoAdjust(sprintf('10cm宽，非自适应\n缺点：两侧空白 '),'x轴','y轴',10);
+utilForFormat.colorbarPlot('值[单位]');
+utilForFormat.dissertationPlotNoAdjust(sprintf('10cm宽，非自适应\n缺点：两侧空白 '),'x轴','y轴',10);
 % 自适应高度
 figure;
 imagesc(xVec,yVec,rand(length(yVec),length(xVec)));
 axis equal;
 xlim([min(xVec),max(xVec)]);
 ylim([min(yVec),max(yVec)]);
-UtilForFormat.colorbarPlot('值[单位]');
-UtilForFormat.dissertationPlot('10cm宽，自适应','x轴','y轴',10);
+utilForFormat.colorbarPlot('值[单位]');
+utilForFormat.dissertationPlot('10cm宽，自适应','x轴','y轴',10);
